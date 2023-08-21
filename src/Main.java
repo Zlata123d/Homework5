6817d4286378186502ac31b89eb13c139cbb6a5c
123 <zlata18_00@mail.ru>
2023-08-21 23:04:42 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     task2();
         task3();}
     public static void task1(){
+        System.out.println("Задача 1");
     int clientOS=0;
     int clientAndroid=1;
     if (clientOS ==0  ){
@@ -14,6 +15,7 @@ public class Main {
 
 
 }} public static void task2() {
+        System.out.println("Задача 2");
         int clientOS = 1;
         int yearOfRelease = 2016;
         if (clientOS ==0 ) {
@@ -29,6 +31,35 @@ public class Main {
             }
         }
     }public static void task3() {
+        System.out.println("Задача 3");
+ int leapYear=2021;
+ switch (leapYear){
+        case 2010:
+        case 2011:
+        case 2012:
+System.out.println("Невисокосный год");
+break;
+     case 2013:
+         System.out.println("Високосный год");
+         break;
+     case 2014:
+     case 2015:
+     case 2016:
+         System.out.println("Невисокосный год");
+         break;
+     case 2017:
+         System.out.println("Високосный год");
+         break;
+     case 2018:
+     case 2019:
+     case 2020:
+         System.out.println("Невисокосный год");
+         break;
+     case 2021:
+         System.out.println("Високосный год");
+         break;
+     default: System.out.println("Такого года не сущетсвует");
+
 
     }
-}
+}}
