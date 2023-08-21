a1316ab4f2693ab7fc36925b04faa19e3bbe7e2d
123 <zlata18_00@mail.ru>
2023-08-21 22:49:24 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args){
         task1();
-       }
+    task2();
+        task3();}
     public static void task1(){
     int clientOS=0;
     int clientAndroid=1;
@@ -11,6 +12,23 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
-    }
 
-}}
+}} public static void task2() {
+        int clientOS = 1;
+        int yearOfRelease = 2016;
+        if (clientOS ==0 ) {
+            if (yearOfRelease < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }else if (clientOS ==1 ){
+            if ( yearOfRelease <2015){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else{ System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+    }public static void task3() {
+
+    }
+}
