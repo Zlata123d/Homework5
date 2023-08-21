d1dc4b0b1c46153944606ae5b9555a7cbf116b3d
123 <zlata18_00@mail.ru>
2023-08-21 22:24:15 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        task1();
+       }
+    public static void task1(){
+    int clientOS=0;
+    int clientAndroid=1;
+    if (clientOS ==0  ){
+        System.out.println("Установите версию приложения для iOS по ссылке");
+        if (clientAndroid ==1){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+    }
+
+}}
