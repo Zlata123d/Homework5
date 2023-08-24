a161470bce3af7dd19602178c29bded9efdc27b1
123 <zlata18_00@mail.ru>
2023-08-24 19:13:31 +0300

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args){
         task1();
     task2();
-        task3();}
+        task3();
+    task4();
+    task5();}
     public static void task1(){
         System.out.println("Задача 1");
     int clientOS=0;
@@ -62,4 +64,63 @@ break;
 
 
     }
-}}
+}
+  public static void task4() {
+    System.out.println("Задача 4");
+    int deliveryDistance = 95;
+    if (deliveryDistance == 20){
+        System.out.println("Потребуется дней: 1");
+            if (deliveryDistance >=20 || deliveryDistance <=60){
+                System.out.println("Потребуется дней: 2");
+            }
+        }
+
+}
+ public static void task5(){
+        System.out.println("Задача 5");
+     int monthNumber=12;
+     switch (monthNumber){
+         case 1:
+             System.out.println("Январь");
+             break;
+         case 2:
+             System.out.println("Февраль");
+             break;
+         case 3:
+             System.out.println("Март");
+             break;
+         case 4:
+             System.out.println("Апрель");
+             break;
+         case 5:
+             System.out.println("Май");
+             break;
+         case 6:
+             System.out.println("Июнь");
+             break;
+         case 7:
+             System.out.println("Июль");
+             break;
+         case 8:
+             System.out.println("Август");
+             break;
+         case 9:
+             System.out.println("Сентябрь");
+             break;
+         case 10:
+             System.out.println("Октябрь");
+             break;
+         case 11:
+             System.out.println("Ноябр");
+             break;
+         case 12:
+             System.out.println("Декабрь");
+             break;
+
+         default: System.out.println("Такого месяца не сущетсвует");
+
+
+     }
+ }
+ }
+
